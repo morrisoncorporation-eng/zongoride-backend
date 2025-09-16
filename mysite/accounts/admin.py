08631f4d222b781174a82ee26a3b619df370fa50
kwamorrison <kwamorrison@gmail.com>
2025-09-16 08:47:56 +0000

    added core app

--- mysite/accounts/admin.py
+++ mysite/accounts/admin.py
@@ -1,7 +1,24 @@
 
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
+
+def generate_jwt_tokens(modeladmin, request, queryset):
+    """
+    Admin action to generate JWT refresh and access tokens for selected users.
+    """
+    for user in queryset:
+        refresh = RefreshToken.for_user(user)
+        message = (
+            f"Tokens for user: {user.username}<br>"
+            f"<strong>Access Token:</strong> {str(refresh.access_token)}<br>"
+            f"<strong>Refresh Token:</strong> {str(refresh)}"
+        )
+        messages.success(request, message)
+
+generate_jwt_tokens.short_description = "Generate JWT tokens for selected users"
+
 
 class UserAdmin(BaseUserAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
@@ -9,6 +26,7 @@ class UserAdmin(BaseUserAdmin):
     search_fields = ('username', 'first_name', 'last_name', 'email')
     ordering = ('username',)
     filter_horizontal = ('groups', 'user_permissions')
+    actions = [generate_jwt_tokens] # Add the custom action here
 
 # Re-register UserAdmin
 admin.site.unregister(User)
